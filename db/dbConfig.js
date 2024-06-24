76cdf602cdeddf0c135ba6fc17d033e3e18ab780
yonatan1 <162271220+yonatan1@users.noreply.github.com>
2024-06-24 18:57:54 -0500

    updated env

--- db/dbConfig.js
+++ db/dbConfig.js
@@ -3,7 +3,7 @@ const mysql2 = require("mysql2");
 const dbConnection = mysql2.createPool({
   user: process.env.USER,
   database: process.env.DATABASE,
-  host: "local host",
+  host: process.env.HOST,
   password: process.env.PASSWORD,
   connectionLimit: 10,
 });
